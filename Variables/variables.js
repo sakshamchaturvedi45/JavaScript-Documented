785560f243878ea387f1ab7e24a70012e9ceba4e
Roy Pr <chaturvedisaksham75@gmail.com>
2025-10-11 21:16:17 +0530

    my first commit

---
+++ Variables/variables.js
@@ -0,0 +1,13 @@
+// let fullName = 'Saksham Chaturvedi';
+// let isCool = true;
+// let totalPets = 0;      
+// let password = 'Sak@12345678';
+
+// console.log('Full Name:', fullName);
+// console.log('Is Cool:', isCool);
+// console.log('Total Pets:', totalPets);
+// console.log('Password:', password);
+console.log('Hello, World!');
+let num = BigInt(4);
+
+console.log(num);
