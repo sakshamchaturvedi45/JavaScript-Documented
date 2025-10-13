f362eb9d216bef5477f555a0479cebaa928e78b0
Roy Pr <chaturvedisaksham75@gmail.com>
2025-10-13 09:21:17 +0530

    functions and bit of operators

--- Variables/variables.js
+++ Variables/variables.js
@@ -7,16 +7,76 @@
 // console.log('Is Cool:', isCool);
 // console.log('Total Pets:', totalPets);
 // console.log('Password:', password);
-console.log('Hello, World!');
-let num = BigInt(4);
+// console.log('Hello, World!');
+// let num = BigInt(4);
+// console.log(num);
 
-console.log(num);
-let favoriateFood = 'Taco';
-let uniqueId = 423743487094;
+// let favoriateFood = 'Taco';
+// let uniqueId = 423743487094;
+// let firstName = 'Saksham';
+// console.log(typeof firstName);
 
-let firstName = 'Saksham';
-console.log(typeof firstName);
-firstName = 200;
-console.log(typeof firstName);
-firstName = true;
-console.log(typeof firstName);
+// firstName = 200;
+// console.log(typeof firstName);
+// firstName = true;
+
+// console.log(typeof firstName);
+
+// dynamic typing in js
+// let bookName = {
+//     name: 'Initialization',
+//     author: 'Jimmy Cook',
+//     pages: 168
+// };
+// console.log(bookName);
+
+// let productColors = ['blue', 'green','yellow','black','orange'];
+// console.log(productColors.length);
+
+// Functions
+// function sayHi(name,batchnum) {
+
+// console.log('Hi! '+ name +', '+ batchnum);
+// }
+// sayHi('Saksham Chaturvedi',1816);
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+// console.log( multiply(21,33));
+
+// Arithmetic operaters
+//  let num1 = 57;
+//  let num2 = 20;
+//  const Bike = num1 % num2;
+
+//  console.log(Bike);
+
+// comparison operators
+// let num1 = 45;
+// let num2 = 20;
+// const Bike = num1 > num2;
+
+// console.log(Bike);
+
+// let num1 = 45;
+// let num2 = 45;
+// const Bike = num1 <= num2;
+
+// console.log(Bike);
+
+// loose equality (converts the data type)
+// let a = 10;
+// let b = '10';
+
+// console.log(a == b);
+
+// strict equality (does not convert)
+// let a = 20;
+// let b = '20';
+// console.log(a === b);
+
+let age = 16;
+const candrink = age >= 18 ? 'hell yeah' : 'grow up kid';
+
+console.log(candrink);
